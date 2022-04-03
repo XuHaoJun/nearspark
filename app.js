4279e1b0dbfe00303693c5c2c7b2a6a1dd3845c1
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-04-03 19:47:53 +0900

    Add express server code

---
+++ app.js
@@ -0,0 +1,32 @@
+const lambda = require("./index");
+const express = require("express");
+
+const app = express();
+
+app.get("/_healthz", function(req, res) {
+  res.send("1");
+});
+
+app.get("*", (req, res) => {
+  const url = req.url.substring(1);
+  console.log({ query: req.query, url });
+
+  lambda.handler(
+    {
+      queryStringParameters: req.query,
+      pathParameters: { url }
+    },
+    null,
+    async function(something, callback) {
+      console.log("callback: ", callback);
+      res
+        .status(callback.statusCode)
+        .header(callback.headers)
+        .send(callback.body);
+    }
+  );
+});
+
+app.listen(5000, function() {
+  console.log("listening on :5000");
+});
