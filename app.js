932822b740691912f75e0ee364512c4982cde311
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-04-28 19:33:03 +0900

    Fix for working on hubs

--- app.js
+++ app.js
@@ -8,31 +8,28 @@ const app = express();
 // });
 
 app.get("/thumbnail/:b64url", function(req, res) {
-  console.log("req.url",req.url);
-  try{
+  try {
     lambda.handler(
       {
         queryStringParameters: req.query,
         pathParameters: {
-          url: req.url.replace("/thumbnail/", "")
+          url: req.path.replace("/thumbnail/", "")
         }
       },
       null,
       async function(something, callback) {
-        console.log("callback: ", callback.statusCode, callback.headers);
-
         res
           .status(callback.statusCode)
           .header(callback.headers)
-          .send(Buffer.from(callback.body, 'base64'))
+          .send(callback.body);
       }
-    )
-  } catch (error){
+    );
+  } catch (error) {
     console.error(error);
-    res.status(400)
+    res.status(400);
   }
 });
 
-app.listen(5000, function() {
-  console.log("listening on :5000");
+app.listen(5002, function() {
+  console.log("listening on :5002");
 });
